9449fce836634747d0b8f31420b67e93e0ef756a
Roger Dornelles <rsd.santo@outlook.com>
2025-07-08 13:14:33 -0300

    teste do eslint, pretier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,55 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+import globals from 'globals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      prettier: pluginPrettier
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      // Regras principais
+      'prettier/prettier': 'error',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-vars': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+      // Suas regras personalizadas
+      quotes: ['error', 'single'],
+      semi: ['error', true],
+      'max-len': ['error', { code: 120 }],
+      'padding-line-between-statements': ['error', { blankLine: 'always', prev: 'function', next: 'function' }],
+      'sort-imports': ['error', { ignoreDeclarationSort: true }],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'prefer-arrow-callback': 'error',
+      indent: ['error', 2]
+    }
+  },
+  prettier
 ];
-
-export default eslintConfig;
---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+export default {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  arrowParens: 'avoid'
+};
