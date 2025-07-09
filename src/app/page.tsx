d3d1cebb0b31ad2b2c56e24b984df2b4eff0ed99
Roger Dornelles <rsd.santo@outlook.com>
2025-07-08 21:38:56 -0300

    teste do husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
 
 const Home = () => {
-  const teste = 'teste 2';
-  const teste1 = 'teste 2';
+  const teste = 'teste';
+  const teste1 = 'teste 1';
   const teste2 = 'teste 2';
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
