f25678898947b3f7e0c0d6fd33e61dd6ec34447b
Roger Dornelles <rsd.santo@outlook.com>
2025-07-10 20:40:18 -0300

    teste de pre-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 import Image from 'next/image';
-import { useState } from 'react';
 
 const Home = () => {
-  const [um, setUm] = useState();
-  const teste = 'teste';
-  const teste1 = 'teste 1';
-  const teste2 = 'testes 2';
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
